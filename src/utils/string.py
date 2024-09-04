f3333e795db771ca70a1a079cbd93ae4ca071b73
WaterGenie35 <thirdwatergenie@gmail.com>
2024-09-05 02:53:12 +0700

    Add solution to project euler problem 17

--- src/utils/string.py
+++ src/utils/string.py
@@ -1,2 +1,122 @@
+from typing import Dict
+
+
 def is_palindrome(s: str) -> bool:
     return s == s[::-1]
+
+
+# https://en.wikipedia.org/wiki/Long_and_short_scales#Comparison
+HUMANIZE_POWERS_STEP = {
+    3: "thousand",
+    6: "million",
+    9: "billion",
+    12: "trillion",
+    15: "quadrillion",
+    18: "quintillion",
+    21: "sextillion",
+    24: "septillion",
+    27: "octillion",
+    30: "nonillion",
+}
+
+HUMANIZE_DIGIT = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+}
+
+HUMANIZE_TENS_LT_20 = {
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+}
+
+HUMANIZE_TENS_GTE_20 = {
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+}
+
+
+def humanize_number(n: int, powers_step: Dict[int, str] = HUMANIZE_POWERS_STEP) -> str:
+    # Supports up to "nonillion" (10**30) for now
+    # E.g. for (12_000_000_345 * 10**30) + 14_011,
+    # the 12_000_000_345 will be humanized as as "twelve billion three hundred
+    # and forty-five nonillion" (as opposed to twelve duodecillion and three
+    # hundred and forty-five nonillion), plus the remaining "fourteen thousand
+    # and eleven".
+    parts = []
+    if n < 10:
+        return HUMANIZE_DIGIT[n]
+
+    if n < 20:
+        return HUMANIZE_TENS_LT_20[n]
+
+    if n < 100:
+        # 21 to 99 separates tenth and unit words with a hyphen
+        tenth_digit = n // 10
+        parts.append(HUMANIZE_TENS_GTE_20[tenth_digit * 10])
+
+        unit_digit = n % 10
+        if unit_digit != 0:
+            parts.append(HUMANIZE_DIGIT[unit_digit])
+
+        return "-".join(parts)
+
+    if n < 1_000:
+        # 100 to 999 separates hundredth and tenth words with "and"
+        hundredth_digit = n // 100
+        parts.append(f"{HUMANIZE_DIGIT[hundredth_digit]} hundred")
+
+        remaining_digits = n % 100
+        if remaining_digits != 0:
+            parts.append(humanize_number(remaining_digits))
+
+        return " and ".join(parts)
+
+    # Separates every 1_000 powers with comma and "and"
+    #  - Use oxford comma
+    #  - Just "and" with no comma if there's only 1 such power
+    powers = sorted(powers_step.keys(), reverse=True)
+
+    largest_step = 10 ** powers[0]
+    remaining = n
+    if n >= largest_step:
+        overflow_digits = n // largest_step
+        remaining %= largest_step
+        parts.append(f"{humanize_number(overflow_digits, powers_step)} {powers_step[powers[0]]}")
+
+    for power in powers[1:]:
+        powerth_digits = remaining // 10**power
+        if powerth_digits != 0:
+            parts.append(f"{humanize_number(powerth_digits)} {powers_step[power]}")
+        remaining %= 10**power
+
+    if remaining != 0:
+        parts.append(humanize_number(remaining))
+
+    if len(parts) <= 2:
+        return " and ".join(parts)
+
+    non_last_parts = ", ".join(parts[:-1])
+    last_part = parts[-1]
+    return f"{non_last_parts}, and {last_part}"
--- tests/utils/test_string.py
+++ tests/utils/test_string.py
@@ -1,4 +1,4 @@
-from utils.string import is_palindrome
+from utils.string import humanize_number, is_palindrome
 
 
 def test_is_palindrome():
@@ -9,3 +9,27 @@ def test_is_palindrome():
     assert not is_palindrome("racecars")
     assert not is_palindrome("12345678")
     assert not is_palindrome("foo 123 321 bar")
+
+
+def test_humanize_number():
+    assert humanize_number(0) == "zero"
+    assert humanize_number(2) == "two"
+    assert humanize_number(13) == "thirteen"
+    assert humanize_number(45) == "forty-five"
+    assert humanize_number(678) == "six hundred and seventy-eight"
+    assert humanize_number(9_001) == "nine thousand and one"
+    # The "and" in 209 is just separating the hundredth and remaining digits
+    # and doesn't affect the "and" separating every 1_000 powers.
+    assert humanize_number(209_001) == "two hundred and nine thousand and one"
+    assert humanize_number(12_003_405) == "twelve million, three thousand, and four hundred and five"
+    assert humanize_number(102_000_034_000) == "one hundred and two billion and thirty-four thousand"
+    # Test overflow (powers step up to "million")
+    short_powers_step = {3: "thousand", 6: "million"}
+    assert humanize_number(2_000_000_000, short_powers_step) == "two thousand million"
+    assert humanize_number(3_000_000_000_050, short_powers_step) == "three million million and fifty"
+    assert humanize_number(3_000_020_000_050, short_powers_step) == "three million and twenty million and fifty"
+    # Not quite clear on how repeated overflow works in English, arbitrary for now by just using recursive calls
+    # Change this as we decide on the usage
+    assert humanize_number(3_140_020_017_050, short_powers_step) == (
+        "three million, one hundred and forty thousand, and twenty million, seventeen thousand, and fifty"
+    )
