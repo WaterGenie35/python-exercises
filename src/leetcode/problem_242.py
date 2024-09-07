71660e1cba8469b9888643875dfc5b79ac8f55bc
WaterGenie35 <thirdwatergenie@gmail.com>
2024-09-08 00:13:35 +0700

    Add solution to leetcode problem 242

---
+++ src/leetcode/problem_242.py
@@ -0,0 +1,39 @@
+def is_anagram(s: str, t: str) -> bool:
+    """
+    Solution to Project Euler problem
+    https://neetcode.io/problems/is-anagram
+    https://leetcode.com/problems/valid-anagram/
+
+    Check if two strings are anagrams of each other.
+    Two strings are anagrams of each other if they contain the same characters
+    in any order.
+    """
+    if len(s) != len(t):
+        return False
+
+    char_freq = {}
+    for char in s:
+        if char in char_freq:
+            char_freq[char] += 1
+        else:
+            char_freq[char] = 1
+
+    pair_freq = {}
+    for char in t:
+        if char not in char_freq:
+            return False
+        if char in pair_freq:
+            pair_freq[char] += 1
+            if pair_freq[char] > char_freq[char]:
+                return False
+        else:
+            pair_freq[char] = 1
+
+    return True
+
+
+def test_solution():
+    assert is_anagram("racecar", "carrace")
+
+    assert not is_anagram("jar", "jam")
+    assert not is_anagram("jar", "jarr")
