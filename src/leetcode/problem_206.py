c13fd39f631e2ce5db854248f0de59662667a496
WaterGenie35 <thirdwatergenie@gmail.com>
2024-09-16 21:57:20 +0700

    Add solution to leetcode problem 206

---
+++ src/leetcode/problem_206.py
@@ -0,0 +1,25 @@
+from utils.typing import Nullable
+
+from leetcode.list import ListNode
+
+
+def reverse_list(head: Nullable[ListNode]) -> Nullable[ListNode]:
+    """
+    Solution to LeetCode problem 206
+    https://neetcode.io/problems/reverse-a-linked-list
+    https://leetcode.com/problems/reverse-linked-list/
+
+    Given the head of a singly linked list, return the reversed list.
+    """
+    current = head
+    new_head = None
+    while current is not None:
+        new_head = ListNode(val=current.val, next=new_head)
+        current = current.next
+    return new_head
+
+
+def test_solution():
+    assert reverse_list(ListNode.from_list([])) == ListNode.from_list([])
+    assert reverse_list(ListNode.from_list([1, 2, 3, 4, 5])) == ListNode.from_list([5, 4, 3, 2, 1])
+    assert reverse_list(ListNode.from_list([3, 2, -5, 0, 2])) == ListNode.from_list([2, 0, -5, 2, 3])
