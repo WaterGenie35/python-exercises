cd9eefedb747016cadf1b26a4568af123d8d5232
WaterGenie35 <thirdwatergenie@gmail.com>
2024-10-05 12:45:02 +0700

    Divide problems into sub folders

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from importlib import import_module
 
 def main():
     parser = ArgumentParser(description="Just some generic programming exercises using Python.")
-    parser.add_argument("exercise", choices=["project_euler"])
+    parser.add_argument("exercise", choices=['project_euler'])
     parser.add_argument("problem_number", type=int)
     parser.add_argument("-p", "--profile", action='store_true', default=False)
     args = parser.parse_args()
@@ -16,8 +16,14 @@ def main():
     print(f"Problem:\t{args.problem_number}")
 
     module_name = f"{args.exercise}.problem_{args.problem_number}"
+    if args.exercise == 'project_euler':
+        problem_group = (args.problem_number - 1) // 50
+        group_start = (50 * problem_group) + 1
+        group_end = group_start + 50 - 1
+        group_dir = f"{group_start}-{group_end}"
+        module_name = f"{args.exercise}.{group_dir}.problem_{args.problem_number}"
     if importlib.util.find_spec(module_name):
-        problem = import_module(f"{args.exercise}.problem_{args.problem_number}")
+        problem = import_module(module_name)
         if args.profile:
             cProfile.runctx('problem.solution()', None, locals())
         else:
