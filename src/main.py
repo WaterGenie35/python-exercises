53a0173b18332f3d720433b5d6bca16da95d8c78
WaterGenie35 <thirdwatergenie@gmail.com>
2024-10-14 16:06:26 +0700

    Also separate leetcode solutions into sub-modules

--- src/main.py
+++ src/main.py
@@ -15,13 +15,11 @@ def main():
     print(f"Exercise:\t{args.exercise}")
     print(f"Problem:\t{args.problem_number}")
 
-    module_name = f"{args.exercise}.problem_{args.problem_number}"
-    if args.exercise == 'project_euler':
-        problem_group = (args.problem_number - 1) // 50
-        group_start = (50 * problem_group) + 1
-        group_end = group_start + 50 - 1
-        group_dir = f"problems_{group_start}_to_{group_end}"
-        module_name = f"{args.exercise}.{group_dir}.problem_{args.problem_number}"
+    problem_group = (args.problem_number - 1) // 50
+    group_start = (50 * problem_group) + 1
+    group_end = group_start + 50 - 1
+    group_dir = f"problems_{group_start}_to_{group_end}"
+    module_name = f"{args.exercise}.{group_dir}.problem_{args.problem_number}"
     if importlib.util.find_spec(module_name):
         problem = import_module(module_name)
         if args.profile:
