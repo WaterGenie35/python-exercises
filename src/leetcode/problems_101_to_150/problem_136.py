902576ad268df69edc9270a701cbd2ae804b2563
WaterGenie35 <thirdwatergenie@gmail.com>
2024-10-14 16:12:30 +0700

    Add solution to leetcode problem 136

---
+++ src/leetcode/problems_101_to_150/problem_136.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def single_number(nums: List[int]) -> int:
+    """
+    Solution to LeetCode problem
+    https://neetcode.io/problems/single-number
+    https://leetcode.com/problems/single-number/
+    """
+    nums_xor = nums[0]
+    for num in nums[1:]:
+        nums_xor ^= num
+    return nums_xor
+
+
+def test_solution():
+    assert single_number([2, 2, 1]) == 1
+    assert single_number([4, 1, 2, 1, 2]) == 4
+    assert single_number([1]) == 1
