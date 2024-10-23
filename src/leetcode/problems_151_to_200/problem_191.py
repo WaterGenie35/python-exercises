556a9fcca400c98c9badccc7af63e8906cff8ffe
WaterGenie35 <thirdwatergenie@gmail.com>
2024-10-23 22:54:07 +0700

    Add solution to leetcode problem 191 (neetcode 150: bit manipulation)

---
+++ src/leetcode/problems_151_to_200/problem_191.py
@@ -0,0 +1,16 @@
+def hamming_weight(n: int) -> int:
+    """
+    Solution to LeetCode problem
+    https://neetcode.io/problems/number-of-one-bits
+    https://leetcode.com/problems/number-of-1-bits/
+    """
+    binary = bin(n)
+    count = 0
+    for bit in binary[2:]:
+        count += int(bit)
+    return count
+
+
+def test_solution():
+    assert hamming_weight(11) == 3
+    assert hamming_weight(128) == 1
