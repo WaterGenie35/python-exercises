7b11b89e292075b10a4aec37d0df321d3bb0e9da
WaterGenie35 <thirdwatergenie@gmail.com>
2024-10-25 14:37:11 +0700

    Add solution to leetcode problem 338 (neetcode 150: bit manipulation)

---
+++ src/leetcode/problems_351_to_400/problem_338.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def count_bits(n: int) -> List[int]:
+    """
+    Solution to LeetCode problem
+    https://neetcode.io/problems/counting-bits
+    https://leetcode.com/problems/counting-bits/
+    """
+    num_bits = []
+    for num in range(0, n + 1):
+        num_bits.append(sum(int(bit) for bit in bin(num)[2:]))
+    return num_bits
+
+
+def test_solution():
+    assert count_bits(2) == [0, 1, 1]
+    assert count_bits(5) == [0, 1, 1, 2, 1, 2]
