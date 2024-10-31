3104b38ea7e455400e477048a8d16ebf56aa75b1
WaterGenie35 <thirdwatergenie@gmail.com>
2024-10-31 10:36:12 +0700

    Add solution to leetcode problem 43 (neetcode 150: math & geometry)

---
+++ src/leetcode/problems_1_to_50/problem_43.py
@@ -0,0 +1,39 @@
+DIGIT_FROM = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+STRING_FROM = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+def multiply(num1: str, num2: str) -> str:
+    """
+    Solution to LeetCode problem
+    https://neetcode.io/problems/multiply-strings
+    https://leetcode.com/problems/multiply-strings/
+
+    No built-in library to convert between int and str.
+    """
+    int1 = 0
+    for char in num1:
+        digit = DIGIT_FROM[char]
+        int1 *= 10
+        int1 += digit
+
+    int2 = 0
+    for char in num2:
+        digit = DIGIT_FROM[char]
+        int2 *= 10
+        int2 += digit
+
+    product_str = ""
+    remaining = int1 * int2
+    if remaining == 0:
+        return "0"
+    while remaining > 0:
+        digit = remaining % 10
+        remaining //= 10
+        product_str = STRING_FROM[digit] + product_str
+
+    return product_str
+
+
+def test_solution():
+    assert multiply("0", "0") == "0"
+    assert multiply("123", "456") == "56088"
